c9bb96891d96e16d72b075cab31de3ac2e905353
James Burton <j.burton@brighton.ac.uk>
2020-05-18 16:10:46 +0100

    Tweak

--- src/Arith/Eval.hs
+++ src/Arith/Eval.hs
@@ -5,7 +5,7 @@ module Arith.Eval
 import Arith.Types
 
 eval :: Exp -> Env -> Maybe Int
-eval (Val v)       env = Just v
+eval (Val v)       _    = Just v
 eval (Id s)        env = lookup s env
 eval (Plus e1 e2)  env = (+) <$> (eval e1 env) <*> (eval e2 env)
 eval (Minus e1 e2) env = (-) <$> (eval e1 env) <*> (eval e2 env)
