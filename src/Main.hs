cc7c66b3981a17fe7101a370ed373dcb8cc85666
James Burton <j.burton@brighton.ac.uk>
2021-12-20 13:28:55 +0000

    Fixed args

--- src/Main.hs
+++ src/Main.hs
@@ -5,7 +5,7 @@ Entry point for the arith package
 
 Run it from cabal like this:
 
-arith$ cabal run arith -- "2+2"
+arith$ cabal run arith "2+2"
 4
 
 or if you want to use identifiers, supply an enironment to look them up in:
@@ -22,11 +22,21 @@ import System.Environment ( getArgs )
 import Arith.Parse        ( parseExp )
 import Arith.Eval         ( eval )
 
+usage :: String
+usage = "enter an arithmetic expression in quotes followed by an optional environment in quotes. \n \
+        \ E.g. \n \
+        \ > arith \"2+x\" \"[(\\\"x\\\",2)]\" \n \
+        \ 4 "
+        
 main :: IO ()
-main = do (e:env:_) <- getArgs
-          case parse parseExp "arithmetic" e of
-            Left e -> print e
-            Right x -> case eval x (read env) of
-              Nothing -> putStrLn "error"
-              (Just i) -> print i
+main = do args <- getArgs
+          if length args == 0
+            then putStrLn usage
+            else case parse parseExp "arithmetic" (head args) of
+                   Left e -> print e
+                   Right x -> do let env = if length args > 1 then (read (head (drop 1 args))) else []
+                                 case eval x env of
+                                   Nothing -> putStrLn "error"
+                                   (Just i) -> print i
+
                           
